32493468c0c8835e3cbe9507cda15c58e99b8dc3
Julian Lau <julian.lau@aol.com>
2022-04-18 21:42:48 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <topnav />
     <router-view/>
   </div>
 </template>
+
+<script>
+import topnav from '@/components/TopNav.vue'
+
+export default {
+  components: {
+    topnav
+  }
+}
+</script>
 
 <style>
 #app {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,94 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main" :style="`background-color: ${generatedColor}`">
+    <div class="color">
+      <div class="center">
+        <h1 :style="`color: ${invertedColor}`">
+          <span @click="copyHex">
+            {{ generatedColor }}
+            <img class="copy" src="../../public/img/copy.png" alt="copy" />
+            <br>
+            <span v-if="copiedHex" class="copied">
+              copied
+            </span>
+          </span>
+          <br>
+          <span @click="copyRgb" class="is-rgb">
+            {{ rgbValue }}
+            <img class="copy" src="../../public/img/copy.png" alt="copy" />
+            <br>
+            <span v-if="copiedRgb" class="copied">
+              copied
+            </span>
+          </span>
+        </h1>
+      </div>
+      <br>
+      <div class="bottom-conter">
+        <button @click="generateColor" class="button is-secondary is-color-button">
+          New Color
+          <i class="fas fa-brush" />
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data () {
+    return  {
+      generatedColor: 'red',
+      copiedHex: false,
+      copiedRgb: false 
+    }
+  },
+  created () {
+    this.generateColor()
+  },
+  computed: {
+    invertedColor() {
+      return '#' + ("000000" + (0xFFFFFF ^ parseInt(this.generatedColor.substring(1),16)).toString(16)).slice(-6);
+    },
+    rgbValue() {
+      const rgb = this.generatedColor.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+             ,(m, r, g, b) => '#' + r + r + g + g + b + b)
+            .substring(1).match(/.{2}/g)
+            .map(x => parseInt(x, 16))
+      
+      return `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`
+    }
+  },
+  methods: {
+    generateColor () {
+      let letters = "0123456789ABCDEF"
+      let color = '#'
+      for (var i = 0; i < 6; i++) {
+         color += letters[(Math.floor(Math.random() * 16))]
+      }
+      this.generatedColor = color
+    },
+    copyHex () {
+      if (navigator.clipboard) {
+        navigator.clipboard.writeText(this.generatedColor)
+      }
+      this.copiedHex = true
+      setTimeout(() => {
+        this.copiedHex = false
+      }, 500)
+    },
+    copyRgb () {
+      if (navigator.clipboard) {
+        navigator.clipboard.writeText(this.rgbValue)
+      }
+      this.copiedRgb = true
+      setTimeout(() => {
+        this.copiedRgb = false
+      }, 500)
+    }
   }
 }
 </script>
